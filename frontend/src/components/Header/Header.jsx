c2433a58176456192506297ddcf6cb08306b3528
EssirKhadija <essirkhadija@gmail.com>
2025-09-28 11:24:04 +0100

    t push --force update header component

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     <div className='header'>
       <div className="header-contents">
         <h2>Order your favorite food here</h2>
-        <p>Chose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time. </p>
+        <p>Chose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience. One delicious meal at a time. </p>
         <button>View Menu</button>
       </div>
     </div>
